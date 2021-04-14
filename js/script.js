5dee1f919a9661ee2317d6c50f74e45d63476641
Paul Bodenstab <bodenstabp@gmail.com>
2021-04-13 20:36:37 -0400

    shirt field selction fix

--- js/script.js
+++ js/script.js
@@ -5,41 +5,41 @@ const shirtFieldDesign = document.querySelector('#design');
 const shirtFieldColor = document.querySelector('#color');
 const activities = document.querySelector('#activities');
 const activitiesTotal = document.querySelector('#activities-cost');
+const paymentOptions = document.querySelector('#payment')
+const creditCard = document.querySelector('#credit-card')
+const payPal = document.querySelector('#paypal')
+const bitcoin = document.querySelector('#bitcoin')
+const submit = document.querySelector('button [type="submit"')
 
 let currentSum = 0;
 
-console.log(activitiesTotal);
 
 // On load controls
 nameField.focus();
 titleFieldDescription.style.display = 'none';
 shirtFieldColor.disabled = true;
+paymentOptions.selectedIndex = 1;
+payPal.style.display = 'none';
+bitcoin.style.display = 'none';
+
 
 
 // Job field controls
-titleField.addEventListener ( 'change', e => e.target.value === 'other' ? titleFieldDescription.style.display = 'block' : titleFieldDescription.style.display = 'none' )
+titleField.addEventListener ( 'change', e => e.target.value === 'other' ? titleFieldDescription.style.display = 'block' : titleFieldDescription.style.display = 'none' );
 
 
 // Shirt field controls
-function shirtColorSelector(shirtType) {
+function shirtColorSelector() {
     for ( let i = 0; i < shirtFieldColor.children.length; i++) {
-        if ( shirtFieldColor.children[i].innerHTML.includes(shirtType) ) {
-            shirtFieldColor.children[i].style.display = 'initial'
-        } else {
-            shirtFieldColor.children[i].style.display = 'none'
-        }
+        shirtFieldColor.children[i].getAttribute('data-theme') === shirtFieldDesign.value ? shirtFieldColor.children[i].style.display = 'initial' : shirtFieldColor.children[i].style.display = 'none';
     }
+    shirtFieldColor.selectedIndex = 0;
 }
 
 shirtFieldDesign.addEventListener ( 'change', e => {
     shirtFieldColor.disabled = false;
     shirtFieldColor.children[0].innerHTML = 'Choose your color';
-    if ( e.target.value === 'js puns' ) {
-        shirtColorSelector('JS Puns')
-    }
-    if ( e.target.value === 'heart js' ) {
-        shirtColorSelector('I')
-    }
+    shirtColorSelector()
 })
 
 
@@ -50,12 +50,35 @@ activities.addEventListener('change', e => {
     e.target.checked === true ? currentSum += price : currentSum -= price;
     activitiesTotal.innerHTML = `Total: $${currentSum}`
 
-    // Activity time controls
+    // Activity checkbox controls
     for (let i = 0; i < e.target.parentNode.parentNode.children.length; i++) {
-        if ( e.target.checked && ( e.target.parentNode.parentNode.children[i].children[0].getAttribute('data-day-and-time') === e.target.getAttribute('data-day-and-time') && e.target.parentNode.parentNode.children[i].children[0] !== e.target ) ){
-            e.target.parentNode.parentNode.children[i].children[0].disabled = true;
-        } else if ( e.target.checked === false && ( e.target.parentNode.parentNode.children[i].children[0].getAttribute('data-day-and-time') === e.target.getAttribute('data-day-and-time') && e.target.parentNode.parentNode.children[i].children[0] !== e.target )) {
-            e.target.parentNode.parentNode.children[i].children[0].disabled = false;
+        let currentTarget = e.target.parentNode.parentNode.children[i].children[0]
+        if ( e.target.checked && ( currentTarget.getAttribute('data-day-and-time') === e.target.getAttribute('data-day-and-time') && currentTarget !== e.target ) ){
+            currentTarget.disabled = true;
+        } else if ( e.target.checked === false && currentTarget.getAttribute('data-day-and-time') === e.target.getAttribute('data-day-and-time')) {
+            currentTarget.disabled = false;
         }
+    }
+})
+
+
+// Payment info controls
+paymentOptions.addEventListener ('change', e => {
+    switch ( paymentOptions.value ) {
+        case 'credit-card' :
+            creditCard.style.display = 'block'
+            payPal.style.display = 'none';
+            bitcoin.style.display = 'none';
+            break;
+        case 'paypal' :
+            paypal.style.display = 'block'
+            creditCard.style.display = 'none';
+            bitcoin.style.display = 'none';
+            break;
+        case 'bitcoin' :
+            bitcoin.style.display = 'block'
+            creditCard.style.display = 'none';
+            paypal.style.display = 'none';
+            break;
     }
 })
